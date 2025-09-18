9183a042991e6fd0f74c58dc1688aa508806db8b
laugh0608 <laugh608@163.com>
2025-09-18 22:05:49 +0800

    refactor: 将框架更改为NET9

---
+++ HtriToExcel/HtriToExcel.cs
@@ -0,0 +1,24 @@
+﻿using HTRICalc;
+
+namespace HtriToExcel;
+
+public class HtriToExcel(HeatExchangerNetwork htriNetWork)
+{
+    // 定义一个 HTRI NetWork 服务器，这是 HTRI 自带的对外 API 接口
+    private HeatExchangerNetwork _htriNetWork = htriNetWork;
+
+    public string Test()
+    {
+        try
+        {
+            // 启动一个 HTRI NetWork 服务器实例
+            _htriNetWork = new HeatExchangerNetwork();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        return "HTRI Success";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HtriToExcel
+
+HtriToExcel is a Python script that converts HTRI (Heat Transfer Research, Inc.) data files into Excel format. This tool is useful for engineers and researchers who work with HTRI data and need to analyze or visualize it in Excel.
+
+* .NET 9
+* HTRI 9.0
+
 使用 .NET 通过 COM 调用 HTRI 9 进行输入、运行和报表导出。
+
+> 正在开发中...
