f4d8e9d1ff3fdbb6c5289fa13bcbb9a1258bf87a
laugh0608 <laugh608@163.com>
2025-09-21 18:02:43 +0800

    feat: 实现自定义API路由特性
    - 根据API版本来生成不同的OpenAPI的JSON文档
    - Scaler根据不同的文档进行渲染

--- HtriToExcel.Api/Controllers/WeatherForecastController.cs
+++ HtriToExcel.Api/Controllers/WeatherForecastController.cs
@@ -1,37 +1,37 @@
+using System.Data.SqlTypes;
+using HtriToExcel.Api.Filter;
 using Microsoft.AspNetCore.Mvc;
 using Scalar.AspNetCore;
 
-namespace HtriToExcel.Api.Controllers
+namespace HtriToExcel.Api.Controllers;
+
+[ApiController]
+// [Route("api/internal/[controller]/[action]")]
+[Route("api/[controller]")]
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]/[action]")]
-    public class WeatherForecastController : ControllerBase
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    private readonly ILogger<WeatherForecastController> _logger = logger;
+
+    // [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet]
+    // [Tags("Test API")] // 如果不加则默认为 Controller 名称
+    // [EndpointGroupName("internal")] // CustomRoute 已经集成了 GroupName
+    [CustomRoute(CustomApiVersion.ApiVersions.Internal, "", "Get")]
+    // [EndpointSummary("测试 API")]
+    // [Badge("V1", BadgePosition.Before, "#007bff")]
+    public IEnumerable<WeatherForecast> Get()
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        [Tags("Test API")]
-        [EndpointSummary("测试 API")]
-        [Badge("V1", BadgePosition.Before, "#007bff")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
-        }
     }
 }
--- HtriToExcel.Api/Program.cs
+++ HtriToExcel.Api/Program.cs
@@ -5,49 +5,50 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// options => options.AddScalarTransformers() 是 Scaler 扩展，用来添加自定义路由等功能
-builder.Services.AddOpenApi(options => options.AddScalarTransformers());
+// 对应 Scaler 的多个 API 文档
+builder.Services.AddOpenApi("V2"); // 最终发布 V2
+builder.Services.AddOpenApi("V1Beta"); // 测试 V1Beta，默认
+builder.Services.AddOpenApi("Internal"); // 内部使用 Internal
+builder.Services.AddOpenApi(options =>
+{
+    // Scaler 扩展
+    options.AddScalarTransformers();
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // 本来下面这几个关于 API 文档的配置都在这里面，但是为了一直能渲染所以移出去了
+    // .NET 9 默认的 OpenAPI
+    app.MapOpenApi();
+
+    // 注入 Scaler 依赖，Api Web UI 管理
+    // 文档地址：https://guides.scalar.com/scalar/scalar-api-references/integrations/net-aspnet-core/integration
+    // 默认路由地址为：/scalar ，自定义路由地址为：/ApiDocs
+    // 自定义路由方法：app.MapScalarApiReference("/ApiDocs", options => {});
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("HtriToExcel API") // 标题
+            .WithTheme(ScalarTheme.BluePlanet) // 主题
+            // .WithSidebar(false) // 关闭侧边栏
+            .WithDarkMode() // 默认使用黑暗模式
+            .WithDefaultOpenAllTags(false); // 是否展开所有标签栏
+        // 自定义查找 Open API 文档地址
+        // options.WithOpenApiRoutePattern("/openapi/{documentName}.json");
+        // 设置默认的 Http 客户端
+        options.WithDefaultHttpClient(ScalarTarget.Node, ScalarClient.HttpClient);
+
+        // 自定义多个版本 API 文档集合
+        options
+            .AddDocument("V2", "V2 API", "openapi/V2.json") // 最终发布 v2
+            .AddDocument("V1Beta", "V1Beta API", "openapi/V1Beta.json", isDefault: true) // 测试 v1beta，默认
+            .AddDocument("Internal", "Internal API", "openapi/Internal.json"); // 内部使用 internal
+    });
 }
-
-// .NET 9 默认的 OpenAPI
-app.MapOpenApi();
-
-// 注入 Scaler 依赖，Api Web UI 管理，文档地址：https://guides.scalar.com/scalar/scalar-api-references/integrations/net-aspnet-core/integration
-// 默认路由地址为：/scalar ，自定义路由地址为：/ApiDocs
-// 自定义路由方法：app.MapScalarApiReference("/ApiDocs", options => {});
-app.MapScalarApiReference(options =>
-{
-    options
-        .WithTitle("HtriToExcel API") // 标题
-        .WithTheme(ScalarTheme.BluePlanet) // 主题
-        // .WithSidebar(false) // 关闭侧边栏
-        .WithDarkMode() // 是否启用黑暗模式开关
-        .WithDefaultOpenAllTags(); // 是否展开所有标签栏
-    // 自定义查找 Open API 文档地址
-    // options.WithOpenApiRoutePattern("/api-spec/{documentName}.json");
-    // 设置默认的 Http 客户端
-    // options.WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
-});
-
-// 自定义多个版本 API 文档
-// var documents = new[]
-// {
-//     new ScalarDocument("v1", "Production API", "api/v1/openapi.json"),
-//     new ScalarDocument("v2", "Beta API", "api/v2-beta/openapi.json", true),
-//     new ScalarDocument("galaxy", "Galaxy API", "https://registry.scalar.com/@scalar/apis/galaxy/latest?format=json")
-// };
-// app.MapScalarApiReference(options => options.AddDocuments(documents));
-// app.MapPost("/orders", CreateOrder)
-//     .WithBadge("New", BadgePosition.Before, "#28a745")
-//     .WithBadge("Premium", BadgePosition.Before, "#ffc107");
 
 // 允许跨域请求
 app.UseCors(options =>
